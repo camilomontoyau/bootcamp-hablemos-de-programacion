2acb19290eb6cc3e2d1e3a4d5fe3a0df9f5d58d2
Camilo <camilo@Mateos-MacBook-Pro.local>
2020-03-10 06:34:31 -0500

    fetch delete

--- ajax/ejemplo-fetch.js
+++ ajax/ejemplo-fetch.js
@@ -1,20 +1,39 @@
 const listaUsuarios = document.getElementById("body-usuarios");
 const boton = document.getElementById("boton");
 const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const pais = document.getElementById("pais");
 let usuarios = [];
+let botonesEliminar = null;
 
 function render() {
   const usuariosRender = usuarios
-    .map(usuario => `<tr><td>${usuario.nombre}</td></tr>`)
+    .map((usuario, indice) => {
+      return `<tr>  
+        <td>${usuario.nombre ? usuario.nombre : 'vacio'}</td>
+        <td>${usuario.apellido ? usuario.apellido : 'vacio'}</td>
+        <td>${usuario.pais ? usuario.pais : 'vacio'}</td>
+        <td><button class="eliminar" data-indice=${indice}  >Eliminar</button></td>
+      </tr>`
+    })
     .join("");
   console.log(usuariosRender);
   listaUsuarios.innerHTML = usuariosRender;
+  botonesEliminar = document.getElementsByClassName('eliminar');
+  Array.from(botonesEliminar).forEach(botonEliminar => {
+    botonEliminar.onclick = eliminarUnUsuario;
+  });
 }
 
-function enviarDatos() {
-  const datos = {nombre: nombre.value};
+function enviarDatos(e) {
+  e.preventDefault();
+  const datos = {
+    nombre: nombre.value, 
+    apellido: apellido.value, 
+    pais: pais.value
+  };
   fetch('https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios', {
-      method: 'POST', // or 'PUT'
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
@@ -27,6 +46,20 @@ function enviarDatos() {
     })
 }
 
+function eliminarUnUsuario(e) {
+  e.preventDefault();
+  console.log('eliminarUnUsuario', e);
+  fetch(`https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios/${e.target.dataset.indice}`, {
+      method: 'DELETE',
+    })
+    .then((response) => response.json())
+    .then(respuestaJson=>{
+      console.log('respuestaJson', respuestaJson)
+      refrescar();
+    })
+}
+
+
 function refrescar() {
   fetch('https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios')
   .then(response=>response.json())
@@ -36,6 +69,7 @@ function refrescar() {
     render();
   })
 }
+//Create, Read, Update,  Delete, Listar
 
 refrescar();
 
