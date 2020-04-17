53947d9de548bb6ead6015585ee401fabacb33d6
camilomontoyau <camilomontoya2@gmail.com>
2020-04-16 19:42:58 -0500

    actualizar mascotas

--- nodejs/enrutador.js
+++ nodejs/enrutador.js
@@ -19,6 +19,18 @@ module.exports = {
       global.recursos.mascotas.push(data.payload);
       callback(201, data.payload);
     },
+    put: (data, callback) => {
+      if (typeof data.indice !== "undefined") {
+        if (global.recursos.mascotas[data.indice]) {
+          global.recursos.mascotas[data.indice] = data.payload;
+          return callback(200, global.recursos.mascotas[data.indice]);
+        }
+        return callback(404, {
+          mensaje: `mascota con indice ${data.indice} no encontrada`,
+        });
+      }
+      callback(400, { mensaje: "indice no enviado" });
+    },
   },
   noEncontrado: (data, callback) => {
     callback(404, { mensaje: "no encontrado" });
