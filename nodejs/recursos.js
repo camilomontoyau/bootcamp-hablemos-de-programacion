91f18ec8fc4691ca4373f1036f122b80a0c992fe
camilomontoyau <camilomontoya2@gmail.com>
2020-04-20 20:02:09 -0500

    diferencias las mascotas en el nombre

--- nodejs/recursos.js
+++ nodejs/recursos.js
@@ -1,9 +1,9 @@
 module.exports = {
   mascotas: [
-    { tipo: "perro", nombre: "Trosky", dueno: "Camilo" },
-    { tipo: "perro", nombre: "Trosky", dueno: "Camilo" },
-    { tipo: "perro", nombre: "Trosky", dueno: "Camilo" },
-    { tipo: "perro", nombre: "Trosky", dueno: "Camilo" },
-    { tipo: "perro", nombre: "Trosky", dueno: "Camilo" },
+    { tipo: "perro", nombre: "Trosky0", dueno: "Camilo" },
+    { tipo: "perro", nombre: "Trosky1", dueno: "Camilo" },
+    { tipo: "perro", nombre: "Trosky2", dueno: "Camilo" },
+    { tipo: "perro", nombre: "Trosky3", dueno: "Camilo" },
+    { tipo: "perro", nombre: "Trosky4", dueno: "Camilo" },
   ],
 };
