474a551480a72116250c865797e9d3e8a2ea7652
camilomontoyau <camilomontoya2@gmail.com>
2020-04-23 19:26:29 -0500

    refactor rutas

--- nodejs/enrutador.js
+++ nodejs/enrutador.js
@@ -1,53 +1,11 @@
+const recursos = require("./recursos");
+const mascotas = require("./rutas/mascotas");
+
 module.exports = {
   ruta: (data, callback) => {
     callback(200, { mensaje: "esta es /ruta" });
   },
-  mascotas: {
-    get: (data, callback) => {
-      if (typeof data.indice !== "undefined") {
-        console.log("handler mascotas", { data });
-        if (global.recursos.mascotas[data.indice]) {
-          return callback(200, global.recursos.mascotas[data.indice]);
-        }
-        return callback(404, {
-          mensaje: `mascota con indice ${data.indice} no encontrada`,
-        });
-      }
-      callback(200, global.recursos.mascotas);
-    },
-    post: (data, callback) => {
-      global.recursos.mascotas.push(data.payload);
-      callback(201, data.payload);
-    },
-    put: (data, callback) => {
-      if (typeof data.indice !== "undefined") {
-        if (global.recursos.mascotas[data.indice]) {
-          global.recursos.mascotas[data.indice] = data.payload;
-          return callback(200, global.recursos.mascotas[data.indice]);
-        }
-        return callback(404, {
-          mensaje: `mascota con indice ${data.indice} no encontrada`,
-        });
-      }
-      callback(400, { mensaje: "indice no enviado" });
-    },
-    delete: (data, callback) => {
-      if (typeof data.indice !== "undefined") {
-        if (global.recursos.mascotas[data.indice]) {
-          global.recursos.mascotas = global.recursos.mascotas.filter(
-            (_mascota, indice) => indice != data.indice
-          );
-          return callback(204, {
-            mensaje: `elemento con indice ${data.indice} eliminado`,
-          });
-        }
-        return callback(404, {
-          mensaje: `mascota con indice ${data.indice} no encontrada`,
-        });
-      }
-      callback(400, { mensaje: "indice no enviado" });
-    },
-  },
+  mascotas: mascotas(recursos.mascotas),
   noEncontrado: (data, callback) => {
     callback(404, { mensaje: "no encontrado" });
   },
--- nodejs/index.js
+++ nodejs/index.js
@@ -1,9 +1,5 @@
 const http = require("http");
 const requestHandler = require("./request-handler");
-const recursos = require("./recursos");
-
-global.recursos = recursos;
-
 const server = http.createServer(requestHandler);
 
 server.listen(5000, () => {
