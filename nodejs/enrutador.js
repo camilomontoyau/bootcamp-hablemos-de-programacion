1f074b1574504e1b57efc465477b2e542379b74b
camilomontoyau <camilomontoya2@gmail.com>
2020-04-23 19:40:51 -0500

    ruta /veterinarias

--- nodejs/enrutador.js
+++ nodejs/enrutador.js
@@ -1,11 +1,13 @@
 const recursos = require("./recursos");
 const mascotas = require("./rutas/mascotas");
+const veterinarias = require("./rutas/veterinarias");
 
 module.exports = {
   ruta: (data, callback) => {
     callback(200, { mensaje: "esta es /ruta" });
   },
   mascotas: mascotas(recursos.mascotas),
+  veterinarias: veterinarias(recursos.veterinarias),
   noEncontrado: (data, callback) => {
     callback(404, { mensaje: "no encontrado" });
   },
--- nodejs/recursos.js
+++ nodejs/recursos.js
@@ -6,4 +6,10 @@ module.exports = {
     { tipo: "perro", nombre: "Trosky3", dueno: "Camilo" },
     { tipo: "perro", nombre: "Trosky4", dueno: "Camilo" },
   ],
+  veterinarias: [
+    { nombre: "Alexandra", apellido: "Perez", documento: "1234567890" },
+    { nombre: "Alexander", apellido: "Gómez", documento: "4234569999" },
+    { nombre: "Julián", apellido: "Madrid", documento: "555666777" },
+    { nombre: "Naryie", apellido: "Vasquez", documento: "1000666777" },
+  ],
 };
