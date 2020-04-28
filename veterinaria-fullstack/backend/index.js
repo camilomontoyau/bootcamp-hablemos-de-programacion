ec1191efec54abcca105daa44491552bd414a954
camilomontoyau <camilomontoya2@gmail.com>
2020-04-27 19:59:38 -0500

    actualización enlaces y puerto

--- veterinaria-fullstack/backend/index.js
+++ veterinaria-fullstack/backend/index.js
@@ -2,8 +2,8 @@ const http = require("http");
 const requestHandler = require("./request-handler");
 const server = http.createServer(requestHandler);
 
-server.listen(6000, () => {
+server.listen(5000, () => {
   console.log(
-    "el servidor está escuchando peticiones en http://localhost:6000/"
+    "el servidor está escuchando peticiones en http://localhost:5000/"
   );
 });
