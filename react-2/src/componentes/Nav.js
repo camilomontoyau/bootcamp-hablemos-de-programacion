8486e4b4591f51ec1282a1ea29051a00162aea7c
camilomontoyau <camilomontoya2@gmail.com>
2020-06-04 20:01:55 -0500

    componente Nav

---
+++ react-2/src/componentes/Nav.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+      <a className="navbar-brand" href="#">
+        Veterinaria
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarColor03"
+        aria-controls="navbarColor03"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarColor03">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/index.html">
+              Mascotas<span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/veterinarias.html">
+              Veterinari@s
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/consultas.html">
+              Consultas
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/duenos.html">
+              Dueños
+            </a>
+          </li>
+        </ul>
+        <form className="form-inline">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button
+            className="btn btn-outline-primary my-2 my-sm-0"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
