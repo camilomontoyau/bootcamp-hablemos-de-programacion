1b381605dd9ef51bb5a25b8620c6497e86645e79
camilomontoyau <camilomontoya2@gmail.com>
2020-06-08 19:17:11 -0500

    Importamos actions menu

--- react-2/src/Pagina.js
+++ react-2/src/Pagina.js
@@ -1,38 +1,13 @@
 import React from "react";
 import Nav from "./componentes/Nav";
-import Ejemplo from "./componentes/Ejemplo";
+import ActionsMenu from "./componentes/ActionsMenu";
 
 function Mascotas() {
   return (
     <>
-      <Ejemplo />
       <div className="container">
         <Nav />
-        <div className="actions-menu">
-          <h1>Mascotas</h1>
-          <div className="actions-menu-content">
-            <button
-              type="button"
-              className="btn btn-primary"
-              data-toggle="modal"
-              data-target="#exampleModalCenter"
-            >
-              Nueva
-            </button>
-            <div className="alert alert-danger alert-dismissible" role="alert">
-              <strong>Oops!</strong> Algo hicimos mal, por favor vuelve a
-              intentarlo!.
-              <button
-                type="button"
-                class="close"
-                data-dismiss="alert"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-          </div>
-        </div>
+        <ActionsMenu />
         <table className="table table-stripped table-hover">
           <thead className="thead-dark">
             <tr>
