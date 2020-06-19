64d1590274d5ccad20424e9bf4093cf1f22dd143
camilomontoyau <camilomontoya2@gmail.com>
2020-06-18 20:16:23 -0500

    removi botones html

--- react-2/src/componentes/Tabla/Fila.js
+++ react-2/src/componentes/Tabla/Fila.js
@@ -13,12 +13,6 @@ function Fila({ index, mascota }) {
         <div className="btn-group" role="group" aria-label="Basic example">
           <BotonAccion tipo="editar" />
           <BotonAccion tipo="eliminar" />
-          <button type="button" className="btn btn-info editar">
-            <i class="fas fa-edit"></i>
-          </button>
-          <button type="button" className="btn btn-danger eliminar">
-            <i className="far fa-trash-alt"></i>
-          </button>
         </div>
       </td>
     </tr>
