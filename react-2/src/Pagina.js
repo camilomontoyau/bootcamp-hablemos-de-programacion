40af2a9cb1042615b08a7b348af67e95d97cbc8a
camilomontoyau <camilomontoya2@gmail.com>
2020-06-22 19:46:51 -0500

    Pagina se convierte en componente de clase y tiene un state para mostar u ocultar la modal

--- react-2/src/Pagina.js
+++ react-2/src/Pagina.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import Nav from "./componentes/Nav";
 import ActionsMenu from "./componentes/ActionsMenu";
 import Tabla from "./componentes/Tabla";
 import Modal from "./componentes/Modal";
 
-function Mascotas() {
-  return (
-    <>
-      <div className="container">
-        <Nav />
-        <ActionsMenu />
-        <Tabla />
-        <Modal />
-      </div>
-    </>
-  );
+class Pagina extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mostraModal: false,
+    };
+  }
+
+  cambiarModal = () => {
+    this.setState({ mostraModal: !this.state.mostraModal });
+  };
+
+  // codigo del componente
+
+  // el método render siempre debe ir de último
+  render() {
+    return (
+      <>
+        <div className="container">
+          <Nav />
+          <ActionsMenu cambiarModal={this.cambiarModal} />
+          <Tabla />
+          {this.state.mostraModal && <Modal />}
+        </div>
+      </>
+    );
+  }
 }
 
-export default Mascotas;
+export default Pagina;
