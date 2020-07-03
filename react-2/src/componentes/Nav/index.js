7301b8aa983de7f31f4b0d8abb8ca5fa73d44395
camilomontoyau <camilomontoya2@gmail.com>
2020-07-02 19:44:42 -0500

    estilos search

--- react-2/src/componentes/Nav/index.js
+++ react-2/src/componentes/Nav/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Search from "../Search";
 import "./Nav.css";
 
 function Nav() {
@@ -7,17 +8,6 @@ function Nav() {
       <a className="navbar-brand" href="#">
         Veterinaria
       </a>
-      {/* <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarColor03"
-        aria-controls="navbarColor03"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button> */}
       <div className="collapse navbar-collapse" id="navbarColor03">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
@@ -41,20 +31,7 @@ function Nav() {
             </a>
           </li>
         </ul>
-        <form className="form-inline">
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button
-            className="btn btn-outline-primary my-2 my-sm-0"
-            type="submit"
-          >
-            Search
-          </button>
-        </form>
+        <Search />
       </div>
     </nav>
   );
