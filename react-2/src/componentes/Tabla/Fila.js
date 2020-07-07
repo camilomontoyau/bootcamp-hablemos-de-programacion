ba2aab74105314ac01814f41671b8d4f10c0d7cb
camilomontoyau <camilomontoya2@gmail.com>
2020-07-06 21:22:46 -0500

    Columnas y EvaluarCampo

--- react-2/src/componentes/Tabla/Fila.js
+++ react-2/src/componentes/Tabla/Fila.js
@@ -2,18 +2,31 @@ import React from "react";
 import BotonAccion from "../BotonAccion";
 import "./Fila.css";
 
+const evaluarCampo = ({ entidad, columna }) => {
+  if (columna === "veterinaria") {
+    return `${entidad[columna].nombre} ${entidad[columna].apellido}`;
+  }
+  if (columna === "mascota") {
+    return `${entidad[columna].nombre} (${entidad[columna].tipo})`;
+  }
+  return entidad[columna];
+};
+
 function Fila({
   index,
   entidad,
   editarEntidad = () => {},
   eliminarEntidad = () => {},
+  columnas = [],
 }) {
   return (
     <tr>
       <th scope="row">{index}</th>
-      <td>{entidad.tipo}</td>
-      <td>{entidad.nombre}</td>
-      <td>{entidad.dueno}</td>
+      {columnas.map((columna, _index) => (
+        <td key={`col-${columna}-${_index}`}>
+          {evaluarCampo({ entidad, columna })}
+        </td>
+      ))}
       <td>
         <div className="btn-group" role="group" aria-label="Basic example">
           <BotonAccion tipo="editar" onClick={editarEntidad} index={index} />
--- react-2/src/componentes/Tabla/index.js
+++ react-2/src/componentes/Tabla/index.js
@@ -20,6 +20,7 @@ function Tabla({
             entidad={entidad}
             editarEntidad={editarEntidad}
             eliminarEntidad={eliminarEntidad}
+            columnas={columnas}
           />
         ))}
       </tbody>
