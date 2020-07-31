82e0a6c1b72a73981bb2ff546589e1294af3c3ad
camilomontoyau <camilomontoya2@gmail.com>
2020-07-30 20:42:49 -0500

    comentarios explicación búsqueda

--- node-react/backend/rutas/mascotas.js
+++ node-react/backend/rutas/mascotas.js
@@ -10,18 +10,45 @@ module.exports = function mascotasHandler(mascotas) {
           mensaje: `mascota con indice ${data.indice} no encontrada`,
         });
       }
+
+      // verifico que data.query traiga datos
+      // en tipo o nombre o dueno, esto significa
+      //que el request es una búsqueda
       if (
         data.query &&
         (typeof data.query.nombre !== "undefined" ||
           data.query.tipo !== "undefined" ||
           data.query.dueno !== "undefined")
       ) {
+        // creo un array con las llavees del objeto data query
         const llavesQuery = Object.keys(data.query);
+
+        //clono el array de mascotas que viene de recursos
+        // y este irá guardando los resultados
         let respuestaMascotas = [...mascotas];
+
+        //recorro cada una de las llaves con el fin de filtrar el array de mascotas
+        //según los criterios de búsqueda
         for (const llave of llavesQuery) {
+          // filtro el array de respuestas con el finde solamente
+          // dejar los objetos de mascota que cumplen con los criterios de
+          // búsqueda
           respuestaMascotas = respuestaMascotas.filter((_mascota) => {
+            // creo una expresión regular para que
+            //la busqueda arroje resultados parciales
+            //de lo que se manda como criterio de búsqueda
+            // ejemplo si tipo = 'gat' en el query me devuelve todas las
+            //mascotas con tipo = 'gato'
             const expresionRegular = new RegExp(data.query[llave], "ig");
+
+            // resultado guarda la verificación del string del criterio de búsqueda
+            // y los objetos de mascota, es decir nos dice si el criterio de búsqueda
+            // está o no en el objeto de mascota que estamos evaluando en el momento
             const resultado = _mascota[llave].match(expresionRegular);
+
+            // resultado entrega null cuando no encuentra el criterio de búsqueda
+            // null es falsy por lo tanto el filter ignorará resultado === null
+            // y los que si tengan el criterio de búsqueda entran al array respuestaMascotas
             return resultado;
           });
         }
