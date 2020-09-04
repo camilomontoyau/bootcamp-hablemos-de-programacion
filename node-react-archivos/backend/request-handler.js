3a55a4d9289d7cb690ff226f522afa93bb2a0196
camilomontoyau <camilomontoya2@gmail.com>
2020-09-03 20:16:25 -0500

    verificar si la respuesta es string o json

--- node-react-archivos/backend/request-handler.js
+++ node-react-archivos/backend/request-handler.js
@@ -83,7 +83,13 @@ module.exports = (req, res) => {
     // 4. ejecutar handler (manejador) para enviar la respuesta
     if (typeof handler === "function") {
       handler(data, (statusCode = 200, mensaje) => {
-        const respuesta = JSON.stringify(mensaje);
+        let respuesta = null;
+        if (typeof mensaje === "string") {
+          respuesta = mensaje;
+        }
+        if (typeof mensaje === "object") {
+          respuesta = JSON.stringify(mensaje);
+        }
         res.setHeader("Content-Type", "application/json");
         res.writeHead(statusCode);
         // linea donde realmente ya estamos respondiendo a la aplicación cliente
