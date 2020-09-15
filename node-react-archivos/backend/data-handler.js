60d9254132a33ccb00ab41745e21aa8409391267
camilomontoyau <camilomontoya2@gmail.com>
2020-09-14 19:33:01 -0500

    modificar crear para usar promesas

--- node-react-archivos/backend/data-handler.js
+++ node-react-archivos/backend/data-handler.js
@@ -3,34 +3,20 @@ const path = require("path");
 const directorioBase = path.join(__dirname, "data");
 
 const dataHandler = {
-  crear: (
-    { directorioEntidad = "mascotas", nombreArchivo, datosGuardar },
-    callback
+  crear: async (
+    { directorioEntidad = "mascotas", nombreArchivo, datosGuardar }
   ) => {
-    fs.open(
-      `${directorioBase}/${directorioEntidad}/${nombreArchivo}.json`,
-      "wx",
-      (error, fileDescriptor) => {
-        if (!error && fileDescriptor) {
-          const datosEnString = JSON.stringify(datosGuardar);
-          fs.writeFile(fileDescriptor, datosEnString, (error2) => {
-            if (error2) {
-              return callback(
-                new Error("Error al intentar escribir en el archivo nuevo")
-              );
-            }
-            fs.close(fileDescriptor, (error3) => {
-              if (error3) {
-                return callback(new Error("Error al cerrar archivo"));
-              }
-              callback(false);
-            });
-          });
-        } else {
-          callback(new Error("No se pudo crear el archivo o ya existe"));
-        }
-      }
-    );
+    try {
+      const fileDescriptor = await fs.promises.open(
+        `${directorioBase}/${directorioEntidad}/${nombreArchivo}.json`,
+        "wx"
+      );
+      const datosEnString = JSON.stringify(datosGuardar);  
+      await fs.promises.writeFile(fileDescriptor, datosEnString);
+      return datosGuardar;
+    } catch (error) {
+      return error;
+    }
   },
   obtenerUno: async ({
     directorioEntidad = "mascotas",
