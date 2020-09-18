3b8c00977c48d85bc43e03147f58d376c06f7e5b
camilomontoyau <camilomontoya2@gmail.com>
2020-09-17 19:45:57 -0500

    listar con filtros desde archivos

--- node-react-archivos/backend/rutas/mascotas.js
+++ node-react-archivos/backend/rutas/mascotas.js
@@ -11,64 +11,68 @@ module.exports = function mascotasHandler(mascotas) {
   return {
     get: async (data, callback) => {
       console.log("handler mascotas", { data });
-      if (typeof data.indice !== "undefined") {
-        const _mascota = await obtenerUno({
-          directorioEntidad: "mascotas",
-          nombreArchivo: data.indice,
-        });
-        return callback(200, _mascota);
-      }
+      
 
-      // verifico que data.query traiga datos
-      // en tipo o nombre o dueno, esto significa
-      //que el request es una búsqueda
-      if (
-        data.query &&
-        (data.query.nombre || data.query.tipo || data.query.dueno)
-      ) {
-        // creo un array con las llaves del objeto data query
-        const llavesQuery = Object.keys(data.query);
-
-        //clono el array de mascotas que viene de recursos
-        // y este irá guardando los resultados
-        let respuestaMascotas = [...mascotas];
-
-        // filtro el array de mascotas según los datos que tenga data.query
-        respuestaMascotas = respuestaMascotas.filter((_mascota) => {
-          // variable resultado cambiará a true cuando alguno de los campos de la _mascota esté en los criterios
-          // de búsqueda es decir esté en alguno de los campos de data.query
-          let resultado = false;
-
-          // recorro cada una de las llaves de data.query para verificar cada uno de los campos
-          // de cada mascota e incluirla o no en el resultado final
-          for (const llave of llavesQuery) {
-            // creo una expresión regular para que
-            //la busqueda arroje resultados parciales
-            //de lo que se manda como criterio de búsqueda
-            // ejemplo si tipo = 'gat' en el query me devuelve todas las
-            //mascotas con tipo = 'gato'
-            const expresionRegular = new RegExp(data.query[llave], "ig");
-
-            // resultado acá guarda la verificación de la expresión regular en cada uno de los campos
-            resultado = _mascota[llave].match(expresionRegular);
-
-            // si resultado es diferente a falso o null (.match entrega null cuando no hay match) entonces
-            // rompemos (break) el ciclo for
-            if (resultado) {
-              break;
-            }
-          }
-
-          // null es falsy por lo tanto el filter ignorará resultado === null
-          // y los que si tengan el criterio de búsqueda entran al array respuestaMascotas
-          return resultado;
-        });
-
-        return callback(200, respuestaMascotas);
-      }
+      
       try {
+        if (typeof data.indice !== "undefined") {
+          const _mascota = await obtenerUno({
+            directorioEntidad: "mascotas",
+            nombreArchivo: data.indice,
+          });
+          return callback(200, _mascota);
+        }
+
         const _mascotas = await listar({ directorioEntidad: "mascotas" });
-        callback(200, _mascotas);
+
+        // verifico que data.query traiga datos
+        // en tipo o nombre o dueno, esto significa
+        //que el request es una búsqueda
+        if (
+          data.query &&
+          (data.query.nombre || data.query.tipo || data.query.dueno)
+        ) {
+          // creo un array con las llaves del objeto data query
+          const llavesQuery = Object.keys(data.query);
+
+          //clono el array de mascotas que viene de recursos
+          // y este irá guardando los resultados
+          let respuestaMascotas = [..._mascotas];
+
+          // filtro el array de mascotas según los datos que tenga data.query
+          respuestaMascotas = respuestaMascotas.filter((_mascota) => {
+            // variable resultado cambiará a true cuando alguno de los campos de la _mascota esté en los criterios
+            // de búsqueda es decir esté en alguno de los campos de data.query
+            let resultado = false;
+
+            // recorro cada una de las llaves de data.query para verificar cada uno de los campos
+            // de cada mascota e incluirla o no en el resultado final
+            for (const llave of llavesQuery) {
+              // creo una expresión regular para que
+              //la busqueda arroje resultados parciales
+              //de lo que se manda como criterio de búsqueda
+              // ejemplo si tipo = 'gat' en el query me devuelve todas las
+              //mascotas con tipo = 'gato'
+              const expresionRegular = new RegExp(data.query[llave], "ig");
+
+              // resultado acá guarda la verificación de la expresión regular en cada uno de los campos
+              resultado = _mascota[llave].match(expresionRegular);
+
+              // si resultado es diferente a falso o null (.match entrega null cuando no hay match) entonces
+              // rompemos (break) el ciclo for
+              if (resultado) {
+                break;
+              }
+            }
+
+            // null es falsy por lo tanto el filter ignorará resultado === null
+            // y los que si tengan el criterio de búsqueda entran al array respuestaMascotas
+            return resultado;
+          });
+
+          return callback(200, respuestaMascotas);
+        }
+        return callback(200, _mascotas);
       } catch (error) {
         if (error) {
           console.log(error);
