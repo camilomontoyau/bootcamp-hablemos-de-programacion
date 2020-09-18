a797d2cddb9541f8c0a3d6a7efc61b0fd0c696d1
camilomontoyau <camilomontoya2@gmail.com>
2020-09-17 19:57:41 -0500

    duenos get obtener uno

--- node-react-archivos/backend/rutas/duenos.js
+++ node-react-archivos/backend/rutas/duenos.js
@@ -1,39 +1,53 @@
+const { obtenerUno } = require("../data-handler");
 const { palabraSinAcentos } = require("../util");
 
 module.exports = function duenosHandler(duenos) {
   return {
-    get: (data, callback) => {
+    get: async (data, callback) => {
       console.log("handler duenos", { data });
-      if (typeof data.indice !== "undefined") {
-        if (duenos[data.indice]) {
-          return callback(200, duenos[data.indice]);
-        }
-        return callback(404, {
-          mensaje: `dueno con indice ${data.indice} no encontradO`,
-        });
-      }
-      if (
-        data.query &&
-        (data.query.nombre || data.query.apellido || data.query.documento)
-      ) {
-        const llavesQuery = Object.keys(data.query);
-        let respuestaDuenos = [...duenos];
-        respuestaDuenos = respuestaDuenos.filter((_dueno) => {
-          let resultado = false;
-          for (const llave of llavesQuery) {
-            const busqueda = palabraSinAcentos(data.query[llave]);
-            const expresionRegular = new RegExp(busqueda, "ig");
-            const campoDuenoSinAcentos = palabraSinAcentos(_dueno[llave]);
-            resultado = campoDuenoSinAcentos.match(expresionRegular);
-            if (resultado) {
-              break;
-            }
+      try {
+        if (typeof data.indice !== "undefined") {
+          const _dueno = await obtenerUno({
+            directorioEntidad: "duenos", 
+            nombreArchivo: data.indice
+          });  
+
+          if  (_dueno && _dueno.id) {
+            return callback(200, _dueno);
           }
-          return resultado;
-        });
-        return callback(200, respuestaDuenos);
+          
+          return callback(404, {
+            mensaje: `dueno con id ${data.indice} no encontrado`,
+          });
+        }
+        if (
+          data.query &&
+          (data.query.nombre || data.query.apellido || data.query.documento)
+        ) {
+          const llavesQuery = Object.keys(data.query);
+          let respuestaDuenos = [...duenos];
+          respuestaDuenos = respuestaDuenos.filter((_dueno) => {
+            let resultado = false;
+            for (const llave of llavesQuery) {
+              const busqueda = palabraSinAcentos(data.query[llave]);
+              const expresionRegular = new RegExp(busqueda, "ig");
+              const campoDuenoSinAcentos = palabraSinAcentos(_dueno[llave]);
+              resultado = campoDuenoSinAcentos.match(expresionRegular);
+              if (resultado) {
+                break;
+              }
+            }
+            return resultado;
+          });
+          return callback(200, respuestaDuenos);
+        }
+        callback(200, duenos);
+      } catch (error) {
+        if (error) {
+          console.log(error);
+          return callback(500, { mensaje: error.message });
+        }
       }
-      callback(200, duenos);
     },
     post: (data, callback) => {
       duenos.push(data.payload);
