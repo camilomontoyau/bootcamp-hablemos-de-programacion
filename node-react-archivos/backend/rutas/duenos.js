8b4233291edbf8a23fec7679a2b2feeb282ff4b8
camilomontoyau <camilomontoya2@gmail.com>
2020-09-17 20:44:08 -0500

    eliminacion de duenos guardados en archivos

--- node-react-archivos/backend/rutas/duenos.js
+++ node-react-archivos/backend/rutas/duenos.js
@@ -93,19 +93,25 @@ module.exports = function duenosHandler(duenos) {
       }
       callback(400, { mensaje: "falta id" });
     },
-    delete: (data, callback) => {
+    delete: async (data, callback) => {
       if (typeof data.indice !== "undefined") {
-        if (duenos[data.indice]) {
-          duenos = duenos.filter((_dueno, indice) => indice != data.indice);
-          return callback(204, {
-            mensaje: `elemento con indice ${data.indice} eliminado`,
+        const resultado = await eliminar({
+          directorioEntidad,
+          nombreArchivo: data.indice,
+        });
+        if (resultado.message) {
+          return callback(404, {
+            mensaje: `dueño con id ${data.indice} no encontrado`,
           });
         }
-        return callback(404, {
-          mensaje: `dueno con indice ${data.indice} no encontrado`,
-        });
+
+        if (resultado.mensaje) {
+          return callback(204);
+        }
+
+        return callback(500, { mensaje: "error al eliminar" });
       }
-      callback(400, { mensaje: "indice no enviado" });
+      callback(400, { mensaje: "falta id" });
     },
   };
 };
