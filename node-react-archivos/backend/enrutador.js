5b38f5090278e83986d59273073818c6255ef00b
camilomontoyau <camilomontoya2@gmail.com>
2020-09-28 17:16:55 -0500

    saco handler del objeto enrutador

--- node-react-archivos/backend/enrutador.js
+++ node-react-archivos/backend/enrutador.js
@@ -6,6 +6,14 @@ const consultas = require("./rutas/consultas");
 const fs = require("fs");
 const path = require("path");
 
+const primerNivel = (_data, callback) => {
+  const rutaIndexHtml = path.join(__dirname, "publico", "index.html");
+  const existeArchivo = fs.existsSync(rutaIndexHtml);
+  console.log({ rutaIndexHtml, existeArchivo });
+  const respuesta = fs.createReadStream(rutaIndexHtml);
+  callback(200, respuesta);
+};
+
 module.exports = {
   ruta: (data, callback) => {
     callback(200, { mensaje: "esta es /ruta" });
@@ -15,13 +23,7 @@ module.exports = {
   duenos: duenos(recursos.duenos),
   consultas: consultas(recursos),
   index: {
-    get: (_data, callback) => {
-      const rutaIndexHtml = path.join(__dirname, "publico", "index.html");
-      const existeArchivo = fs.existsSync(rutaIndexHtml);
-      console.log({ rutaIndexHtml, existeArchivo });
-      const respuesta = fs.createReadStream(rutaIndexHtml);
-      callback(200, respuesta);
-    },
+    get: primerNivel,
   },
   noEncontrado: (data, callback) => {
     callback(404, { mensaje: "no encontrado" });
