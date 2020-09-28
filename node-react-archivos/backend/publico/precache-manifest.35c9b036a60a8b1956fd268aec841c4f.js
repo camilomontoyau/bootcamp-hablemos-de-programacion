ac539485e28122e4ed9506f512a080d28954b672
camilomontoyau <camilomontoya2@gmail.com>
2020-09-28 17:11:46 -0500

    copiar archivos build desde react

---
+++ node-react-archivos/backend/publico/precache-manifest.35c9b036a60a8b1956fd268aec841c4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef797fa913a74c138bee59d3369a7c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4cb872685da1b985387",
+    "url": "/static/css/main.4dd8428c.chunk.css"
+  },
+  {
+    "revision": "7dfc502d1c83dc5360c7",
+    "url": "/static/js/2.87dd224f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.87dd224f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4cb872685da1b985387",
+    "url": "/static/js/main.10f0aca6.chunk.js"
+  },
+  {
+    "revision": "c34291ff6df906d86d17",
+    "url": "/static/js/runtime-main.40eab162.js"
+  }
+]);
