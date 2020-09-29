46805f48dc145f23be3aeab04c60eeaa9c1a755b
camilomontoyau <camilomontoya2@gmail.com>
2020-09-28 19:46:47 -0500

    cargando archivos segun nombre archivo

--- node-react-archivos/backend/enrutador.js
+++ node-react-archivos/backend/enrutador.js
@@ -6,6 +6,16 @@ const consultas = require("./rutas/consultas");
 const path = require("path");
 const fs = require("fs");
 
+const handlerArchivos = (data, callback) => {
+  const rutaIndexHtml = path.join(__dirname, "publico", data.ruta);
+  const existeArchivo = fs.existsSync(rutaIndexHtml);
+  if (existeArchivo) {
+    const respuesta = fs.createReadStream(rutaIndexHtml);
+    return callback(200, respuesta);
+  }
+  callback(404, { mensaje: "no encontrado" });
+};
+
 module.exports = {
   ruta: (data, callback) => {
     callback(200, { mensaje: "esta es /ruta" });
@@ -14,16 +24,14 @@ module.exports = {
   veterinarias: veterinarias(recursos.veterinarias),
   duenos: duenos(recursos.duenos),
   consultas: consultas(recursos),
-  index: {
-    get: (_data, callback) => {
-      const rutaIndexHtml = path.join(__dirname, "publico", "index.html");
-      const existeArchivo = fs.existsSync(rutaIndexHtml);
-      if (existeArchivo) {
-        const respuesta = fs.createReadStream(rutaIndexHtml);
-        return callback(200, respuesta);
-      }
-      callback(404, { mensaje: "no encontrado" });
-    },
+  "index.html": {
+    get: handlerArchivos,
+  },
+  "favicon.ico": {
+    get: handlerArchivos,
+  },
+  "manifest.json": {
+    get: handlerArchivos,
   },
   noEncontrado: (data, callback) => {
     callback(404, { mensaje: "no encontrado" });
