ffd6b831ae6c420ad2277629a591393bc589013c
camilomontoyau <camilomontoya2@gmail.com>
2020-09-28 20:15:13 -0500

    archivos estaticos nuevos

---
+++ node-react-archivos/backend/publico/precache-manifest.cc44005398c852909435b84035ba6e00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3b892ef3d3a31c873a576a0f48851d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ceb3ebf56e05f9a5016",
+    "url": "/static/css/main.4dd8428c.chunk.css"
+  },
+  {
+    "revision": "7dfc502d1c83dc5360c7",
+    "url": "/static/js/2.87dd224f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.87dd224f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ceb3ebf56e05f9a5016",
+    "url": "/static/js/main.19410cf0.chunk.js"
+  },
+  {
+    "revision": "c34291ff6df906d86d17",
+    "url": "/static/js/runtime-main.40eab162.js"
+  }
+]);
