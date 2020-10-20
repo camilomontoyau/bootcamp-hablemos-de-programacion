9733fbc68e734daa1d6c154ca62d285e39771d44
camilomontoyau <camilomontoya2@gmail.com>
2020-10-19 19:42:40 -0500

    GET /mascotas/:_id obtener una sola mascota

--- express02/index.js
+++ express02/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
-const { crear, listar, actualizar, eliminar } = require("./data-handler");
+const {
+  crear,
+  listar,
+  actualizar,
+  eliminar,
+  obtenerUno,
+} = require("./data-handler");
 const app = express();
 const port = 5000;
 
@@ -14,6 +20,21 @@ app.get("/", (req, res) => {
 app.get("/mascotas", async (req, res) => {
   const mascotas = await listar({ directorioEntidad: "mascotas" });
   res.status(200).json(mascotas);
+});
+
+app.get("/mascotas/:_id", async (req, res) => {
+  const { _id = null } = req.params;
+  if (!_id) {
+    return res.status(400).json({ mensaje: "Falta el id" });
+  }
+  const mascota = await obtenerUno({
+    directorioEntidad: "mascotas",
+    nombreArchivo: _id,
+  });
+  if (mascota) {
+    res.status(200).json(mascota);
+  }
+  res.status(404).json({ mensaje: "no encontrado" });
 });
 
 app.post("/mascotas", async (req, res) => {
