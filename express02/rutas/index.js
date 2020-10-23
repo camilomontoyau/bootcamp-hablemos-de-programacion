42404b442cbda55429cb39b42bebbdf648c4731d
camilomontoyau <camilomontoya2@gmail.com>
2020-10-22 19:55:59 -0500

    ruta veterinarias + veterinaria nueva

--- express02/rutas/index.js
+++ express02/rutas/index.js
@@ -1,7 +1,9 @@
 const mascotas = require("./mascotas");
 const duenos = require("./duenos");
+const veterinarias = require("./veterinarias");
 
 module.exports = (app) => {
   app.use("/mascotas", mascotas);
   app.use("/duenos", duenos);
+  app.use("/veterinarias", veterinarias);
 };
