0669241eb065b85f9411aa4291e13e4a525310e5
camilomontoyau <camilomontoya2@gmail.com>
2020-10-22 19:57:56 -0500

    directorio rutas consultas

--- express02/rutas/index.js
+++ express02/rutas/index.js
@@ -1,9 +1,11 @@
 const mascotas = require("./mascotas");
 const duenos = require("./duenos");
 const veterinarias = require("./veterinarias");
+const consultas = require("./consultas");
 
 module.exports = (app) => {
   app.use("/mascotas", mascotas);
   app.use("/duenos", duenos);
   app.use("/veterinarias", veterinarias);
+  app.use("/consultas", consultas);
 };
