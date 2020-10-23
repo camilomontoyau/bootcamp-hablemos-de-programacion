8b5abe217b46486ac7dd2d279f96d8ec794cbc26
camilomontoyau <camilomontoya2@gmail.com>
2020-10-22 19:52:42 -0500

    directorio rutas duenos

---
+++ express02/rutas/duenos/index.js
@@ -0,0 +1,87 @@
+const router = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const {
+  crear,
+  listar,
+  actualizar,
+  eliminar,
+  obtenerUno,
+} = require("../../data-handler");
+const entidad = "duenos";
+
+router.get("/", async (req, res) => {
+  if (!entidad) {
+    res.status(404).status({ mensaje: "no encontrado" });
+  }
+  const mascotas = await listar({ directorioEntidad: entidad });
+  res.status(200).json(mascotas);
+});
+
+router.get("/:_id", async (req, res) => {
+  const { _id = null } = req.params;
+  if (!_id) {
+    return res.status(400).json({ mensaje: "Falta el id" });
+  }
+  if (!entidad) {
+    res.status(404).status({ mensaje: "no encontrado" });
+  }
+  const mascota = await obtenerUno({
+    directorioEntidad: entidad,
+    nombreArchivo: _id,
+  });
+  if (mascota) {
+    res.status(200).json(mascota);
+  }
+  res.status(404).json({ mensaje: "no encontrado" });
+});
+
+router.post("/", async (req, res) => {
+  if (!entidad) {
+    res.status(404).status({ mensaje: "no encontrado" });
+  }
+  if (req.body && Object.keys(req.body).length > 0) {
+    const _id = uuidv4();
+    const datosMascotaNueva = { ...req.body, _id };
+    const nuevaMascota = await crear({
+      directorioEntidad: entidad,
+      nombreArchivo: _id,
+      datosGuardar: datosMascotaNueva,
+    });
+    return res.status(200).json(nuevaMascota);
+  }
+  return res.status(400).json({ mensaje: "Falta el body" });
+});
+
+router.put("/:_id", async (req, res) => {
+  const { _id = null } = req.params;
+  if (!_id) {
+    return res.status(400).json({ mensaje: "Falta el id" });
+  }
+  if (!entidad) {
+    res.status(404).status({ mensaje: "no encontrado" });
+  }
+  if (req.body && Object.keys(req.body).length > 0) {
+    const datosActuales = { ...req.body, _id };
+    const mascotaActualizada = await actualizar({
+      directorioEntidad: entidad,
+      nombreArchivo: _id,
+      datosActuales,
+    });
+    return res.status(200).json(mascotaActualizada);
+  }
+  return res.status(400).json({ mensaje: "Falta el body" });
+});
+
+router.delete("/:_id", async (req, res) => {
+  const { _id = null } = req.params;
+  if (!_id) {
+    return res.status(400).json({ mensaje: "Falta el id" });
+  }
+  if (!entidad) {
+    res.status(404).status({ mensaje: "no encontrado" });
+  }
+  await eliminar({ directorioEntidad: entidad, nombreArchivo: _id });
+  return res.status(204).send();
+});
+
+module.exports = router;
--- express02/rutas/index.js
+++ express02/rutas/index.js
@@ -1,5 +1,7 @@
 const mascotas = require("./mascotas");
+const duenos = require("./duenos");
 
 module.exports = (app) => {
   app.use("/mascotas", mascotas);
+  app.use("/duenos", duenos);
 };
