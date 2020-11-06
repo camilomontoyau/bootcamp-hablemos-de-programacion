95cf98e94a79ccae0cce1364ccb0ed6c13bed597
camilomontoyau <camilomontoya2@gmail.com>
2020-11-05 19:38:24 -0500

    controller consultas con metodos genéricos

--- express02/rutas/controllers/consultas/index.js
+++ express02/rutas/controllers/consultas/index.js
@@ -1,87 +1,28 @@
 const router = require("express").Router();
-const { v4: uuidv4 } = require("uuid");
+
 const {
-  crear,
   listar,
+  obtenerUno,
+  crear,
   actualizar,
   eliminar,
-  obtenerUno,
-} = require("../../../data-handler");
+} = require("../genericos");
+
 const entidad = "consultas";
 
-router.get("/", async (req, res) => {
-  if (!entidad) {
-    res.status(404).status({ mensaje: "no encontrado" });
-  }
-  const mascotas = await listar({ directorioEntidad: entidad });
-  res.status(200).json(mascotas);
-});
+const listarHandler = listar(entidad);
+router.get("/", listarHandler);
 
-router.get("/:_id", async (req, res) => {
-  const { _id = null } = req.params;
-  if (!_id) {
-    return res.status(400).json({ mensaje: "Falta el id" });
-  }
-  if (!entidad) {
-    res.status(404).status({ mensaje: "no encontrado" });
-  }
-  const mascota = await obtenerUno({
-    directorioEntidad: entidad,
-    nombreArchivo: _id,
-  });
-  if (mascota) {
-    res.status(200).json(mascota);
-  }
-  res.status(404).json({ mensaje: "no encontrado" });
-});
+const obtenerUnoHandler = obtenerUno(entidad);
+router.get("/:_id", obtenerUnoHandler);
 
-router.post("/", async (req, res) => {
-  if (!entidad) {
-    res.status(404).status({ mensaje: "no encontrado" });
-  }
-  if (req.body && Object.keys(req.body).length > 0) {
-    const _id = uuidv4();
-    const datosMascotaNueva = { ...req.body, _id };
-    const nuevaMascota = await crear({
-      directorioEntidad: entidad,
-      nombreArchivo: _id,
-      datosGuardar: datosMascotaNueva,
-    });
-    return res.status(200).json(nuevaMascota);
-  }
-  return res.status(400).json({ mensaje: "Falta el body" });
-});
+const crearHandler = crear(entidad);
+router.post("/", crearHandler);
 
-router.put("/:_id", async (req, res) => {
-  const { _id = null } = req.params;
-  if (!_id) {
-    return res.status(400).json({ mensaje: "Falta el id" });
-  }
-  if (!entidad) {
-    res.status(404).status({ mensaje: "no encontrado" });
-  }
-  if (req.body && Object.keys(req.body).length > 0) {
-    const datosActuales = { ...req.body, _id };
-    const mascotaActualizada = await actualizar({
-      directorioEntidad: entidad,
-      nombreArchivo: _id,
-      datosActuales,
-    });
-    return res.status(200).json(mascotaActualizada);
-  }
-  return res.status(400).json({ mensaje: "Falta el body" });
-});
+const editarHandler = actualizar(entidad);
+router.put("/:_id", editarHandler);
 
-router.delete("/:_id", async (req, res) => {
-  const { _id = null } = req.params;
-  if (!_id) {
-    return res.status(400).json({ mensaje: "Falta el id" });
-  }
-  if (!entidad) {
-    res.status(404).status({ mensaje: "no encontrado" });
-  }
-  await eliminar({ directorioEntidad: entidad, nombreArchivo: _id });
-  return res.status(204).send();
-});
+const eliminarHandler = eliminar(entidad);
+router.delete("/:_id", eliminarHandler);
 
 module.exports = router;
