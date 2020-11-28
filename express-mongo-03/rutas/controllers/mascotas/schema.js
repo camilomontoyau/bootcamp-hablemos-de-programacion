fe3509758186079fe2aee07d2c0662768f057d70
camilomontoyau <camilomontoya2@gmail.com>
2020-11-28 11:46:40 -0500

    timestamps en los schemas

--- express-mongo-03/rutas/controllers/mascotas/schema.js
+++ express-mongo-03/rutas/controllers/mascotas/schema.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const mascotaSchema = new Schema({
-  tipo: String,
-  nombre: {
-    type: String,
-    required: true,
+const mascotaSchema = new Schema(
+  {
+    tipo: String,
+    nombre: {
+      type: String,
+      required: true,
+    },
+    dueno: {
+      type: String,
+      required: true,
+    },
   },
-  dueno: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("mascotas", mascotaSchema);
