b5358441d48bf149d379d1b16d6c7d236b6c79a6
camilomontoyau <camilomontoya2@gmail.com>
2020-12-14 19:48:13 -0500

    DELETE /mascotas en genericos

--- express-mongo-03/rutas/controllers/mascotas/index.js
+++ express-mongo-03/rutas/controllers/mascotas/index.js
@@ -25,24 +25,7 @@ router.post("/", crearHandler);
 const editarHandler = actualizar({ Modelo: Mascota });
 router.put("/:_id", editarHandler);
 
-// const eliminarHandler = eliminar(entidad);
-router.delete("/:_id", async (req, res) => {
-  try {
-    const { _id = null } = req.params;
-    if (!_id) {
-      return res.status(400).json({ mensaje: "falta id" });
-    }
-    const mascotaBorrada = await Mascota.remove({_id});
-    if  (mascotaBorrada.deletedCount === 1) {
-      return res.status(204).send();
-    }
-    return res
-      .status(500)
-      .json({ mensaje: "no se pudo eliminar el recurso, vuelva a intentar" });
-  } catch (error) {
-    console.log({ error });
-    return res.status(500).json({ mensaje: error.message });
-  }
-});
+const eliminarHandler = eliminar({ Modelo: Mascota });
+router.delete("/:_id", eliminarHandler);
 
 module.exports = router;
