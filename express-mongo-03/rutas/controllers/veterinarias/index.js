f1959675423671ab293b5ba373030d00f4b81d92
camilomontoyau <camilomontoya2@gmail.com>
2020-12-14 19:41:04 -0500

    editar en genéricos

--- express-mongo-03/rutas/controllers/veterinarias/index.js
+++ express-mongo-03/rutas/controllers/veterinarias/index.js
@@ -20,26 +20,8 @@ router.get("/:_id", obtenerUnoHandler);
 const crearHandler = crear({ Modelo: Veterinaria });
 router.post("/", crearHandler);
 
-//const editarHandler = actualizar(entidad);
-
-router.put("/:_id", async (req, res) => {
-  try {
-    const { _id = null } = req.params;
-    const { _id: id, ...datosNuevos } = req.body;
-    if (!_id) {
-      return res.status(400).json({ mensaje: "falta id" });
-    }
-    const veterinariaActualizada = await Veterinaria.findOneAndUpdate(
-      { _id },
-      { $set: datosNuevos },
-      { new: true, runValidators: true }
-    );
-    return res.status(200).json(veterinariaActualizada);
-  } catch (error) {
-    console.log({ error });
-    return res.status(500).json({ mensaje: error.message });
-  }
-});
+const editarHandler = actualizar({ Modelo: Veterinaria });
+router.put("/:_id", editarHandler);
 
 // const eliminarHandler = eliminar(entidad);
 router.delete("/:_id", async (req, res) => {
