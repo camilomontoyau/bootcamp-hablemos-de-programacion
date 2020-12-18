46f34766841351bfb6060545701b6582f9b73428
camilomontoyau <camilomontoya2@gmail.com>
2020-12-17 20:24:49 -0500

    cambios con errores en el PUT de veterinarias

--- express-mongo-03/rutas/controllers/genericos.js
+++ express-mongo-03/rutas/controllers/genericos.js
@@ -83,7 +83,7 @@ const actualizar = function closureEditarEntidad({ Modelo = null }) {
       return res.status(200).json(entidadActualizada);
     } catch (error) {
       console.log({ error });
-      if (error.name === "MongoError" && error.code === 11000) {
+      if (error.code === 11000) {
         return res.status(400).json({
           mensaje: `ya existe otra entidad con el documento ${req.body.documento}!`,
         });
