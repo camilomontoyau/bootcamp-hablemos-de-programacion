24c3efeb564d36184125591b9b88fd75c29049f3
camilomontoyau <camilomontoya2@gmail.com>
2020-12-21 19:54:52 -0500

    middlewareExisteEntidadConMismoDocumentoyDiferenteId

--- express-mongo-03/rutas/controllers/duenos/index.js
+++ express-mongo-03/rutas/controllers/duenos/index.js
@@ -18,11 +18,22 @@ const obtenerUnoHandler = obtenerUno({ Modelo: Dueno });
 router.get("/:_id", obtenerUnoHandler);
 
 const crearHandler = crear({ Modelo: Dueno });
-const middlewareExisteDocumento = existeDocumento({ Modelo: Dueno });
+const middlewareExisteDocumento = existeDocumento({
+  Modelo: Dueno,
+  campos: ["documento"],
+});
 router.post("/", middlewareExisteDocumento, crearHandler);
 
 const editarHandler = actualizar({ Modelo: Dueno });
-router.put("/:_id", editarHandler);
+const middlewareExisteEntidadConMismoDocumentoyDiferenteId = existeDocumento({
+  Modelo: Dueno,
+  campos: ["documento", { operador: "$ne", nombre: "_id" }],
+});
+router.put(
+  "/:_id",
+  middlewareExisteEntidadConMismoDocumentoyDiferenteId,
+  editarHandler
+);
 
 const eliminarHandler = eliminar({ Modelo: Dueno });
 router.delete("/:_id", eliminarHandler);
