2efd2945c8c5ffbaed5ed3517019b1c57117ede4
camilomontoyau <camilomontoya2@gmail.com>
2021-02-02 07:35:23 -0500

    verificar que la mascota exista antes de crear una consulta

--- express-mongo-03/rutas/controllers/consultas/index.js
+++ express-mongo-03/rutas/controllers/consultas/index.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const Consulta = require("./schema");
+const Veterinaria = require("../veterinarias/schema");
+const Mascota = require("../mascotas/schema");
 
 const {
   listar,
@@ -21,7 +23,24 @@ router.get("/:_id", obtenerUnoHandler);
 
 
 const crearHandler = crear({ Modelo: Consulta });
-router.post("/", crearHandler);
+router.post("/", async (req, res) => {
+  const { mascota = null, veterinaria = null } = req.body;
+  const existeVeterinaria = await Veterinaria.exists({ _id: veterinaria });
+  const existeMascota = await Mascota.exists({ _id: mascota });
+  if (existeVeterinaria && existeMascota) {
+    return crearHandler(req, res);
+  }
+  if (!existeVeterinaria) {
+    return res.status(400).json({
+      mensaje: `Veterinari@ con _id ${JSON.stringify(veterinaria)} no existe!`,
+    });
+  }
+  if (!existeMascota) {
+    return res.status(400).json({
+      mensaje: `Mascota con _id ${JSON.stringify(veterinaria)} no existe!`,
+    });
+  }
+});
 
 const editarHandler = actualizar({ Modelo: Consulta });
 router.put("/:_id", editarHandler);
