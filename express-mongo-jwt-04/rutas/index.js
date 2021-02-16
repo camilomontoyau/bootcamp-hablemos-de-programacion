e179a072bb0fc2b21e9b4ba965d53529ecc4747c
camilomontoyau <camilomontoya2@gmail.com>
2021-02-16 07:37:59 -0500

    eliminamos las entidades de duenos y veterinarias porque se pasaron a usuarios

--- express-mongo-jwt-04/rutas/index.js
+++ express-mongo-jwt-04/rutas/index.js
@@ -1,13 +1,9 @@
 const mascotas = require("./controllers/mascotas");
-const duenos = require("./controllers/duenos");
-const veterinarias = require("./controllers/veterinarias");
 const consultas = require("./controllers/consultas");
 const usuarios = require("./controllers/usuarios");
 
 module.exports = (app) => {
   app.use("/mascotas", mascotas);
-  app.use("/duenos", duenos);
-  app.use("/veterinarias", veterinarias);
   app.use("/consultas", consultas);
   app.use("/usuarios", usuarios);
 };
