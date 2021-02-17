a29426a27f7d42bff274c00a5f26057ccdd5aad3
camilomontoyau <camilomontoya2@gmail.com>
2021-02-17 07:35:32 -0500

    controlador + handler del login

--- express-mongo-jwt-04/rutas/controllers/login/index.js
+++ express-mongo-jwt-04/rutas/controllers/login/index.js
@@ -1,23 +1,30 @@
 const createError = require("http-errors");
 const router = require("express").Router();
 const Usuario = require("../usuarios/schema");
+const jwt = require("jsonwebtoken");
+const { manejadorDeErrores } = require("../../../util");
+const SECRET_KEY = process.env.SECRET_KEY;
 
 router.post("/", async (req, res, next) => {
-  const { password = null, email = null } = req.body;
-  let err = null;
-  if (password && password.length > 0 && email && email.length > 0) {
-    const usuario = await Usuario.findOne({ email });
-    if (!usuario) {
-      err = new createError[400]("email o password errados");
+  try {
+    const { password = null, email = null } = req.body;
+    let err = new createError[400]("email o password errados");
+    if (password && password.length > 0 && email && email.length > 0) {
+      let usuario = await Usuario.findOne({ email });
+      if (!usuario) {
+        return next(err);
+      }
+      if (usuario.password === password) {
+        usuario = usuario.toJSON();
+        const { password, ...datosUsuario } = usuario;
+        const token = jwt.sign(datosUsuario, SECRET_KEY, { expiresIn: 5 * 60 });
+        const respuesta = { token, usuario: datosUsuario };
+        return res.status(200).json(respuesta);
+      }
       return next(err);
     }
-    if (usuario.password === password) {
-      // logica para cuando el usuario está logueado correctamente
-    }
-
-    if (existeDueno) {
-      return crearHandler(req, res);
-    }
+  } catch (error) {
+    return manejadorDeErrores({ error, next });
   }
 });
 
--- express-mongo-jwt-04/rutas/index.js
+++ express-mongo-jwt-04/rutas/index.js
@@ -1,9 +1,11 @@
 const mascotas = require("./controllers/mascotas");
 const consultas = require("./controllers/consultas");
 const usuarios = require("./controllers/usuarios");
+const login = require("./controllers/login");
 
 module.exports = (app) => {
   app.use("/mascotas", mascotas);
   app.use("/consultas", consultas);
   app.use("/usuarios", usuarios);
+  app.use("/login", login);
 };
