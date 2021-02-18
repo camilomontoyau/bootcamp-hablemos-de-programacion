9333b212c6ad4966801e5bed753cc258945ae1d8
camilomontoyau <camilomontoya2@gmail.com>
2021-02-18 07:29:05 -0500

    middleware estaAutenticado

--- express-mongo-jwt-04/rutas/index.js
+++ express-mongo-jwt-04/rutas/index.js
@@ -1,11 +1,25 @@
+const createError = require("http-errors");
+const lodash = require("lodash");
 const mascotas = require("./controllers/mascotas");
 const consultas = require("./controllers/consultas");
 const usuarios = require("./controllers/usuarios");
 const login = require("./controllers/login");
 
+
+const estaAutenticado = (req, res, next) => {
+  const auth = lodash.get(req, "headers.authorization", null);
+  if (!auth) {
+    const err = new createError.Unauthorized("Falta el token");
+    next(err);
+  }
+
+  next();
+};
+
 module.exports = (app) => {
+  app.use("/login", login);
+  app.use(estaAutenticado);
   app.use("/mascotas", mascotas);
   app.use("/consultas", consultas);
   app.use("/usuarios", usuarios);
-  app.use("/login", login);
 };
