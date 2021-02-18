39b2027d96dae8abdc6526a71687f90aea0158c2
camilomontoyau <camilomontoya2@gmail.com>
2021-02-18 08:05:05 -0500

    middleware estaAutenticado + jwt.verify

--- express-mongo-jwt-04/rutas/index.js
+++ express-mongo-jwt-04/rutas/index.js
@@ -1,19 +1,33 @@
 const createError = require("http-errors");
 const lodash = require("lodash");
+const jwt = require("jsonwebtoken");
 const mascotas = require("./controllers/mascotas");
 const consultas = require("./controllers/consultas");
 const usuarios = require("./controllers/usuarios");
 const login = require("./controllers/login");
+const { getMaxListeners } = require("./controllers/mascotas/schema");
+const SECRET_KEY = process.env.SECRET_KEY;
 
 
 const estaAutenticado = (req, res, next) => {
-  const auth = lodash.get(req, "headers.authorization", null);
-  if (!auth) {
+  let auth = lodash.get(req, "headers.authorization", null);
+  if (!auth && auth.length) {
     const err = new createError.Unauthorized("Falta el token");
-    next(err);
+    return next(err);
   }
-
-  next();
+  const [_bearer, token] = auth.split(" ");
+  console.log({ auth, _bearer, token });
+  jwt.verify(token, SECRET_KEY, (error, decoded) => {
+    if (error) {
+      console.log({ error });
+      const err = new createError[500]("error al verificar token");
+      return next(err);
+    }
+    if  (decoded) {
+      console.log({  decoded  });
+      next();
+    }
+  });
 };
 
 module.exports = (app) => {
