403c52a0204eb84d07cc3d628628e73e553b821c
camilomontoyau <camilomontoya2@gmail.com>
2021-02-19 07:13:25 -0500

    estaAutenticado pasa a util

--- express-mongo-jwt-04/rutas/index.js
+++ express-mongo-jwt-04/rutas/index.js
@@ -1,34 +1,9 @@
-const createError = require("http-errors");
-const lodash = require("lodash");
-const jwt = require("jsonwebtoken");
 const mascotas = require("./controllers/mascotas");
 const consultas = require("./controllers/consultas");
 const usuarios = require("./controllers/usuarios");
 const login = require("./controllers/login");
-const { getMaxListeners } = require("./controllers/mascotas/schema");
-const SECRET_KEY = process.env.SECRET_KEY;
+const { estaAutenticado } = require("./../util");
 
-
-const estaAutenticado = (req, res, next) => {
-  let auth = lodash.get(req, "headers.authorization", null);
-  if (!auth && auth.length) {
-    const err = new createError.Unauthorized("Falta el token");
-    return next(err);
-  }
-  const [_bearer, token] = auth.split(" ");
-  console.log({ auth, _bearer, token });
-  jwt.verify(token, SECRET_KEY, (error, decoded) => {
-    if (error) {
-      console.log({ error });
-      const err = new createError[500]("error al verificar token");
-      return next(err);
-    }
-    if  (decoded) {
-      console.log({  decoded  });
-      next();
-    }
-  });
-};
 
 module.exports = (app) => {
   app.use("/login", login);
--- express-mongo-jwt-04/util.js
+++ express-mongo-jwt-04/util.js
@@ -1,5 +1,7 @@
 const createError = require("http-errors");
 const lodash = require("lodash");
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = process.env.SECRET_KEY;
 
 const manejadorDeErrores = ({ error, next }) => {
   let err = null;
@@ -27,7 +29,29 @@ const removerPaswordDeRespuestas = (objeto) => {
   return resto;
 };
 
+const estaAutenticado = (req, res, next) => {
+  let auth = lodash.get(req, "headers.authorization", null);
+  if (!auth && auth.length) {
+    const err = new createError.Unauthorized("Falta el token");
+    return next(err);
+  }
+  const [_bearer, token] = auth.split(" ");
+  console.log({ auth, _bearer, token });
+  jwt.verify(token, SECRET_KEY, (error, decoded) => {
+    if (error) {
+      console.log(JSON.stringify({ error }, null, 2));
+      const err = new createError[500]("error al verificar token");
+      return next(err);
+    }
+    if (decoded) {
+      console.log({ decoded });
+      next();
+    }
+  });
+};
+
 module.exports = {
   manejadorDeErrores,
   removerPaswordDeRespuestas,
+  estaAutenticado,
 };
