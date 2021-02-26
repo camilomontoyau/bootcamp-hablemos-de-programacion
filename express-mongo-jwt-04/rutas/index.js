654eadf79a5dbad44aa7fc27211bd688b6031664
camilomontoyau <camilomontoya2@gmail.com>
2021-02-26 07:25:23 -0500

    ejemplo middlewareEstaAutorizado

--- express-mongo-jwt-04/rutas/index.js
+++ express-mongo-jwt-04/rutas/index.js
@@ -3,11 +3,13 @@ const consultas = require("./controllers/consultas");
 const usuarios = require("./controllers/usuarios");
 const login = require("./controllers/login");
 const { estaAutenticado } = require("./../util");
+const { middlewareEstaAutorizado } = require("./controllers/genericos");
 
 
 module.exports = (app) => {
   app.use("/login", login);
   app.use(estaAutenticado);
+  app.use(middlewareEstaAutorizado({ tiposUsuario: ["administrador"] }));
   app.use("/mascotas", mascotas);
   app.use("/consultas", consultas);
   app.use("/usuarios", usuarios);
