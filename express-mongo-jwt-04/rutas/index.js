fbaf6c8bf3868dd2dbce5434dfafff15ace75bfd
camilomontoyau <camilomontoya2@gmail.com>
2021-03-18 08:29:56 -0500

    facebook login

--- express-mongo-jwt-04/rutas/index.js
+++ express-mongo-jwt-04/rutas/index.js
@@ -11,25 +11,36 @@ const {FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, FACEBOOK_CALLBACK_URL} = process.en
 passport.use(
   new FacebookStrategy(
     {
-        clientID: FACEBOOK_APP_ID,
-        clientSecret: FACEBOOK_APP_SECRET,
-        callbackURL: FACEBOOK_CALLBACK_URL,
+      clientID: FACEBOOK_APP_ID,
+      clientSecret: FACEBOOK_APP_SECRET,
+      callbackURL: FACEBOOK_CALLBACK_URL,
     },
-      function  (accessToken, refreshToken, profile, cb) {
-        User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-          return cb(err, user);
-        });
-      }
-    )
+    function (accessToken, refreshToken, profile, cb) {
+      console.log("callback del passport.use");
+      console.log({ accessToken, refreshToken, profile });
+      return cb(false, {
+        firstName: "Camilo",
+        lastName: "Montoya",
+        email: "camilomontoya2@gmail.com",
+      });
+      /* User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+        return cb(err, user);
+      }); */
+    }
+  )
 );
 
 module.exports = (app) => {
   app.use("/login", login);
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.get('/auth/facebook', passport.authenticate('facebook'));
   app.get(
     '/auth/facebook/callback',
     passport.authenticate('facebook', { failureRedirect: '/login' }),
     function(req, res) {
+      console.log("autenticacióon FB exitosa");
+      console.log(req);
       // Successful authentication, redirect home.
       res.redirect('/');
     }
