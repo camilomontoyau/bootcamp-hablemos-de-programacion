18e6fc29f6079c9a21709a36b692e73c8a289d1e
camilomontoyau <camilomontoya2@gmail.com>
2021-03-25 07:35:04 -0500

    remuevo cookie parser y express session

--- express-mongo-jwt-04/index.js
+++ express-mongo-jwt-04/index.js
@@ -1,24 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-const cookieParser = require("cookie-parser");
-const expressSession = require("express-session");
 const conexion = require("./db");
 conexion();
 const rutas = require("./rutas");
 const app = express();
 const port = 5000;
-const SECRET_KEY = process.env.SECRET_KEY;
 
-//acá va la config cookie parser
-app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded());
-//acá va la config de express-session
-app.use(expressSession({
-  secret: SECRET_KEY,
-  resave: true,
-  saveUninitialized: true
-}));
 
 rutas(app);
 
